98a37c679c18848898ecf66746982c43f169ed20
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-13 18:08:06 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231113/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char hurt[3];
+    int deg[3];
+
+    int check = 0;
+
+    cin >> hurt[0] >> deg[0] >> hurt[1] >> deg[1] >> hurt[2] >> deg[2];
+    for(int i = 0; i < 3; i++){
+        if(hurt[0] == 'Y'){
+            if(deg[i] >= 37)
+                check++;
+        }
+    }
+
+    if(check >= 2)
+        cout << "E";
+    else
+        cout << "N";
+
+    return 0;
+}
