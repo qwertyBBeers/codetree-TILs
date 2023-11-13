d45014a62e2c81a1ec534f57083cc32b424b618c
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-13 17:53:37 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231113/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a >= b){
+        if(b >= c)
+            cout << a;
+        else{
+            if(a >= c)
+                cout << a;
+            else
+                cout << c;
+        }
+
+    }
+    else{
+        if(b >= c)
+            cout << b;
+        else
+            cout << c;
+    }
+
+    return 0;
+}
