a606ff8db0022f44f3a5fed5523a4509a1b749fe
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-13 16:58:36 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231113/두 사람/two-person.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    char a1;
+    char b1;
+    cin >> a >> a1 >> b >> b1;
+
+    if((a >= 19 && a1 == 'M')||(b >= 19 && a1 == 'M'))
+        cout << "1";
+
+    return 0;
+}
