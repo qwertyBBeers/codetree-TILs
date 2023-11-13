cb40fe8cdc3c6f0d9d073d5d4336b948a6056c0b
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-13 17:34:55 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231113/윤년인가/is-leap-year.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+    
+    cin >> y;
+
+    if(y % 4 == 0){
+        if(y % 100 == 0){
+            if(y % 400 == 0)
+                cout << "true";
+            else
+                cout << "false";
+        }
+
+        else
+            cout << "true";
+    }
+
+    else
+        cout << "false";
+
+    return 0;
+}
