4ef682ef1d06a971c5fd3030782e2e69471b88b0
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-13 16:54:06 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231113/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[2];
+    int b[2];
+
+    cin >> a[0] >> a[1];
+    cin >> b[0] >> b[1];
+
+    if(a[0] > b[0])
+        cout << "A";
+    else if(b[0] > a[0])
+        cout << "B";
+    else{
+        if(a[1] > b[1])
+            cout << "A";
+        else if(a[1] > b[1])
+            cout << "B";
+    }
+    return 0;
+}
