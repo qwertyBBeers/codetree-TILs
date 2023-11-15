05aa85c08c6af9d4d81bc1af79b3f3f45e4cdb34
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-15 14:11:37 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231115/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, score = 1;
+    cin >> n;
+
+    while(score <= n){
+        cout << score << " ";
+        score++;
+    }
+    return 0;
+}
