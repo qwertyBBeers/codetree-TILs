7e69ede26bf0fc016c70ef6e5d60c60026b3110b
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-15 14:37:20 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231115/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 26;
+    while(n >= 10){
+        cout << n << " ";
+        n--;
+    }
+    return 0;
+}
