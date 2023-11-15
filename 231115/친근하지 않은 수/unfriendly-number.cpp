4ac11dcb074cfdbdfe2794d02984e0258685328c
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-15 15:52:00 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231115/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt = 0;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        if(i % 2 !=0 && i % 3!=0 &&i%5!=0)
+            cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
