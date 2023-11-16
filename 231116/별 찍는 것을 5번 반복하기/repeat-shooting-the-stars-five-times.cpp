15b83f253d7be2cbabf0d8f04a24873f9a295359
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-16 16:37:44 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231116/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
+++ 231116/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    stars();
-    return 0;
-}
-
 void stars(){
     for(int j = 0; j < 5; j++){
         for(int i = 0; i < 10; i++){
@@ -14,4 +8,10 @@ void stars(){
         }
         cout << endl;
     }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    stars();
+    return 0;
 }
