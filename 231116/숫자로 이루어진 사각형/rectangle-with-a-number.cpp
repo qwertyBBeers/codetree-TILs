6244c068a881880ac379aa01d0fb701fa1496d26
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-16 17:07:27 +0900

    Title: 숫자로 이루어진 사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231116/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
+++ 231116/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
-void square(int n){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            score = (i + j)%9;
-            
-            cout << score << " ";
+// n * n 크기의 사각형에 숫자를 출력하는 함수입니다.
+void PrintNum(int n) {
+    int cnt = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << cnt << " ";
+            cnt++;
+            if(cnt == 10)
+                cnt = 1;
         }
         cout << endl;
     }
 }
+
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n;
-    cin >> n;
-    square(n);
+    // 변수 선언 및 입력:
+    int row_num;
+    cin >> row_num;
+
+    PrintNum(row_num);
+
     return 0;
 }
