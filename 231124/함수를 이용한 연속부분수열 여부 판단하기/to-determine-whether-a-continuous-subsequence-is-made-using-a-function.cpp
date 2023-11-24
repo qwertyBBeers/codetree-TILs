333d21a83dac0cac1eabf3db29686d1e1cf3124b
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-24 15:53:07 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231124/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 231124/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
+
+#define MAX_N 100
+
 using namespace std;
 
-bool func(int n1, int n2){
-    int a[100], b[100];
+int n1, n2;
+int a[MAX_N], b[MAX_N];
+
+// n번째 인덱스부터 시작하는 n2길이의 a수열과
+// b수열이 완전히 일치하는지 확인합니다..
+bool IsSame(int n) {
+    for(int i = 0; i < n2; i++)
+        if(a[i + n] != b[i])
+            return false;
+
+    return true;
+}
+
+// b가 a의 연속부분수열인지 확인합니다.
+bool IsSubsequence() {
+    for(int i = 0; i <= n1 - n2; i++)
+        if(IsSame(i))
+            return true;
+    
+    return false;
+}
+
+int main() {
+    // 변수 선언 및 입력:
+    cin >> n1 >> n2;
 
     for(int i = 0; i < n1; i++)
         cin >> a[i];
+    
     for(int i = 0; i < n2; i++)
         cin >> b[i];
 
-    if(n2 > n1){
-        return false;
-    }
-    else{
-        for(int i = 0; i < n1; i++){
-            for(int j = 0; j < n2; j++){
-                if(a[i] == b[j]){
-                    for(int k = 0; k < n2 - j; k++){
-                        if(a[i+k] != b[j+k]){
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            }
-             
-        }
-        return false;
-    }
-}
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int n1, n2;
-    cin >> n1 >> n2;
-
-    if(func(n1, n2) == true)
+    
+    if(IsSubsequence())
         cout << "Yes";
     else
         cout << "No";
