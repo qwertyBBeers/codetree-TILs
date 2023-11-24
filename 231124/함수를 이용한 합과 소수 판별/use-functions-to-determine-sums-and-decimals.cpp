ae07046d1ffbac4825129e498c7023659fdb1c44
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-24 15:11:35 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231124/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
+++ 231124/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -2,36 +2,48 @@
 
 using namespace std;
 
-int function(int a, int b){
-    int cnt = 0;
-    int result = 0;
-
-    for(a; a <= b; a++){
-        for(int i = 2; i < a; i++){
-            if(a%i == 0){
-                break;
-            }
-        }
-        for(int i = 0; i < 3; i++){
-            cnt += a%10;
-            if(a/10 == 0)
-                break;
-            a = a/10;
-        }
-        
-        if(cnt % 2 == 0)
-            result++;
-    }
-
-    return result;
+// 해당 숫자가 소수인지 여부를 반환하는 함수를 작성합니다.
+bool IsPrime(int n) {
+    if(n == 1)
+        return false;
+    
+    for(int i = 2; i < n; i++)
+        if(n % i == 0)
+            return false;
+    
+    return true;
 }
+
+// 자릿수의 합이 짝수인지 여부를 반환하는 함수를 작성합니다.
+bool IsDigitSumEven(int n) {
+    int digit_sum = (n / 100) + ((n / 10) % 10) + (n % 10);
+    if(digit_sum % 2 == 0)
+        return true;
+    
+    return false;
+}
+
+// 해당 숫자가 소수이면서 자릿수의 합이 짝수인지 여부를 반환하는 함수를 작성합니다.
+bool JudgeNum(int n) {
+    if(IsPrime(n) && IsDigitSumEven(n))
+        return true;
+    
+    return false;
+}
+
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 변수 선언 및 입력:
     int a, b;
-    int result = 0;
     cin >> a >> b;
 
-    result = function(a, b);
-    cout << result;
+    int cnt = 0;
+
+    for(int i = a; i <= b; i++) {
+        if(JudgeNum(i))
+            cnt++;
+    }
+
+    cout << cnt;
+
     return 0;
 }
