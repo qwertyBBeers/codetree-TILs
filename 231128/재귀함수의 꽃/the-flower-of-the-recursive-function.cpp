9b189e83efa3df110e78001b3cd39a143156ae5a
HyoungHo Park <98011534+qwertyBBeers@users.noreply.github.com>
2023-11-28 15:42:05 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231128/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void func(int N){
+    if(N == 0)
+        return;
+    cout << N << " ";
+    func(N-1);
+    cout << N << " ";
+
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N;
+    cin >> N;
+
+    func(N);
+    return 0;
+}
